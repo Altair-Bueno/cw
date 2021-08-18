18da415920e0f24b8915113ab18f078eabc36a35
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-08-18 14:38:12 +0200

    Preparing release 1.0

---
+++ bench/bench.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+cd "$GITHUB_WORKSPACE/tests/resources" || exit
+
+export ARGS_LIST_HYPERFINE=$(ls | xargs)
+echo "Byte count"
+hyperfine -m 100 -w 2 -s basic "cw -b $ARGS_LIST_HYPERFINE" "wc -c $ARGS_LIST_HYPERFINE"
+echo "********************************************************************************"
+echo "Word count"
+hyperfine -m 100 -w 2 -s basic "cw -w $ARGS_LIST_HYPERFINE" "wc -w $ARGS_LIST_HYPERFINE"
+echo "********************************************************************************"
+echo "Char count"
+hyperfine -m 100 -w 2 -s basic "cw -c $ARGS_LIST_HYPERFINE" "wc -m $ARGS_LIST_HYPERFINE"
+echo "********************************************************************************"
+echo "Line count"
+hyperfine -m 100 -w 2 -s basic "cw -l $ARGS_LIST_HYPERFINE" "wc -l $ARGS_LIST_HYPERFINE"
+echo "********************************************************************************"
+echo "max length"
+hyperfine -m 100 -w 2 -s basic "cw -L $ARGS_LIST_HYPERFINE" "wc -L $ARGS_LIST_HYPERFINE"
+echo "********************************************************************************"
+echo "Default mode"
+hyperfine -m 100 -w 2 -s basic "cw $ARGS_LIST_HYPERFINE" "wc $ARGS_LIST_HYPERFINE"
+echo "********************************************************************************"
+echo "High load"
+hyperfine -m 100 -w 2 -s basic "cw $ARGS_LIST_HYPERFINE $ARGS_LIST_HYPERFINE $ARGS_LIST_HYPERFINE" "wc $ARGS_LIST_HYPERFINE $ARGS_LIST_HYPERFINE $ARGS_LIST_HYPERFINE"
