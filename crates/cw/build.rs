eeffd16255ffae2546d79b8b689782b921af4cdc
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-08-30 02:13:55 +0200

    Added build script for shell completition

---
+++ crates/cw/build.rs
@@ -0,0 +1,26 @@
+use clap::Shell;
+use clap::App;
+use clap::load_yaml;
+use clap::crate_name;
+use std::env;
+
+
+fn main() {
+    let dir = env::var_os("SHELL_COMPLETIONS_DIR")
+        .or(env::var_os("OUT_DIR"))
+        .or(Some("target/sh".parse().unwrap()));
+
+    let outdir = match dir {
+        None => return,
+        Some(outdir) => outdir,
+    };
+    let _ignore = std::fs::create_dir_all(&outdir);
+    let yaml = load_yaml!("resources/cmdline-clap.yaml");
+    let mut app = App::from_yaml(yaml);
+
+    app.gen_completions(crate_name!(), Shell::Zsh, &outdir);
+    app.gen_completions(crate_name!(), Shell::Bash, &outdir);
+    app.gen_completions(crate_name!(), Shell::PowerShell, &outdir);
+    app.gen_completions(crate_name!(), Shell::Fish, &outdir);
+
+}
