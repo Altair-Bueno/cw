6ba752d2138d5456137d6652ab124f1832b320ed
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-09-24 13:11:33 +0200

    fix: Using `mpsc` to fix https://github.com/Altair-Bueno/cw/issues/15

--- crates/cw/src/commandline/exec_jobs.rs
+++ crates/cw/src/commandline/exec_jobs.rs
@@ -6,6 +6,8 @@ use colored::Colorize;
 
 use libcw::Parser;
 use libcw::Stats;
+use std::option::Option::Some;
+use tokio::task::JoinHandle;
 
 const TOTAL: &str = "total";
 
@@ -13,12 +15,55 @@ pub async fn process_files(v:Vec<&str>, parser: Parser) -> ! {
     // TODO Remove vectors. They allocate memory on the heap that may lead to
     // cache miss
     let size = v.len();
+
+    let (s, mut r) = tokio::sync::mpsc::channel(512);
+    let cloned_vec = v.iter().map(ToString::to_string).collect::<Vec<String>>();
+
+    // Too many spawn?
+    tokio::spawn(async move {
+        for e in cloned_vec.iter() {
+            let cloned_e = e.to_string();
+            let cloned_s = s.clone();
+            let _: JoinHandle<Result<(),std::io::Error>> = tokio::spawn(async move {
+                let file = tokio::fs::File::open(cloned_e).await?;
+                let reader = tokio::io::BufReader::new(file);
+                let res = parser.proccess(reader).await;
+                let _ = cloned_s.send(res).await;
+                Ok(())
+            });
+        }
+    });
+
+    // Buffers
     let mut buff_stderr = tokio::io::BufWriter::new(tokio::io::stderr());
     let mut buff_stdout = tokio::io::BufWriter::new(tokio::io::stdout());
     let s = format!("{} {}",parser.to_string().as_str().blue(),"File(s)\n".blue());
     let _ = buff_stdout.write(s.as_bytes()).await;
-    let mut tasks = Vec::with_capacity(size);
 
+
+    let (code, merged) = {
+        let mut iter = v.iter();
+        let mut code = 0;
+        let mut stats = Stats::default();
+        while let Some(next) = r.recv().await {
+            // Unwrap here is safe
+            let file = iter.next().unwrap();
+            match next {
+                Ok(x) => {
+                    let s = format!("{}{}\n", x, file);
+                    let _ = buff_stdout.write(s.as_bytes()).await;
+                    stats = stats.combine(x);
+                }
+                Err(err) => {
+                    let s = format!("{}: {}\n", file, err);
+                    let _ = buff_stderr.write(s.as_bytes()).await;
+                    code += 1;
+                }
+            }
+        }
+        (code,stats)
+    };
+    /*
     for next in v.iter() {
         let path = next.to_string();
         let parser = parser.clone();
@@ -57,6 +102,7 @@ pub async fn process_files(v:Vec<&str>, parser: Parser) -> ! {
         }
         (code,stats)
     };
+     */
 
     if size > 1 {
         // Total files
