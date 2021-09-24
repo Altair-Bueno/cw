e01b0103e5f4291c30441a5a62e2bfa96d0caba4
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-09-24 13:35:31 +0200

    ref: Reduced number of clones

--- crates/cw/src/commandline/exec_jobs.rs
+++ crates/cw/src/commandline/exec_jobs.rs
@@ -10,31 +10,33 @@ use std::option::Option::Some;
 use tokio::task::JoinHandle;
 
 const TOTAL: &str = "total";
+const MAX_FILE_DESCRIPTORS : usize = 512;
 
 pub async fn process_files(v:Vec<&str>, parser: Parser) -> ! {
     // TODO Remove vectors. They allocate memory on the heap that may lead to
     // cache miss
     let size = v.len();
 
-    let (s, mut r) = tokio::sync::mpsc::channel(512);
+    let (s, mut r) = tokio::sync::mpsc::channel(MAX_FILE_DESCRIPTORS);
     let cloned_vec = v.iter().map(ToString::to_string).collect::<Vec<String>>();
 
-    // Too many spawn?
     tokio::spawn(async move {
-        for e in cloned_vec.iter() {
-            let cloned_e = e.to_string();
+        for e in cloned_vec {
             let cloned_s = s.clone();
-            let _: JoinHandle<Result<(),std::io::Error>> = tokio::spawn(async move {
-                let file = tokio::fs::File::open(cloned_e).await?;
-                let reader = tokio::io::BufReader::new(file);
-                let res = parser.proccess(reader).await;
-                let _ = cloned_s.send(res).await;
-                Ok(())
+            tokio::spawn(async move {
+                let _ = match tokio::fs::File::open(e).await {
+                    Ok(file) => {
+                        let reader = tokio::io::BufReader::new(file);
+                        let res = parser.proccess(reader).await;
+                        cloned_s.send(res).await
+                    }
+                    Err(err) => cloned_s.send(Err(err)).await
+                };
             });
         }
     });
 
-    // Buffers
+    // stdio buffers
     let mut buff_stderr = tokio::io::BufWriter::new(tokio::io::stderr());
     let mut buff_stdout = tokio::io::BufWriter::new(tokio::io::stdout());
     let s = format!("{} {}",parser.to_string().as_str().blue(),"File(s)\n".blue());
@@ -63,47 +65,6 @@ pub async fn process_files(v:Vec<&str>, parser: Parser) -> ! {
         }
         (code,stats)
     };
-    /*
-    for next in v.iter() {
-        let path = next.to_string();
-        let parser = parser.clone();
-        let handle = tokio::spawn(async move {
-            let file = tokio::fs::File::open(&path).await?;
-            let buff = tokio::io::BufReader::new(file);
-            parser.proccess(buff).await
-        });
-        tasks.push(handle)
-    }
-    let (code,merged) = {
-        let mut code = 0;
-        let mut stats = Stats::default();
-        for (task,file) in tasks.into_iter().zip(v) {
-            match task.await {
-                Ok(Ok(x)) => {
-                    let s = format!("{}{}\n", x, file);
-                    let _ = buff_stdout.write(s.as_bytes()).await;
-                    stats = stats.combine(x);
-                },
-                Ok(Err(err)) => {
-                    let s = format!("{}: {}\n", file, err);
-                    let _ = buff_stderr.write(s.as_bytes()).await;
-                    code +=1;
-                },
-                // Althought it should always recive a result from `tokio`, i
-                // prefer to handle this error
-                #[cfg(not(debug_assertions))]
-                _ => {},
-                #[cfg(debug_assertions)]
-                some => {
-                    let s = format!("{:?}",some);
-                    let _ = buff_stderr.write(s.as_bytes()).await;
-                },
-            }
-        }
-        (code,stats)
-    };
-     */
-
     if size > 1 {
         // Total files
         let s = format!(
