ce3fb78e2fd9deaf20fdfb0a206af686c27dd39a
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-01-20 09:36:23 +0100

    ref: moved functions to appropriated module

--- crates/cw/src/config.rs
+++ crates/cw/src/config.rs
@@ -1,46 +1,45 @@
-use clap::{Parser,ArgGroup};
-use libcw::config::{Encoding, LineBreak};
+use clap::{ArgGroup, Parser};
 
-#[derive(Parser,Debug)]
+use libcw::config::{Encoding, LineBreak};
+use libcw::Parser as CwParser;
+
+#[derive(Parser, Debug, Clone)]
 #[clap(author, version, about, long_about = None)]
 #[clap(
-    group(
-        ArgGroup::new("input")
-            .required(false)
-            .args(&["from-stdin", "files"])
-    )
+group(
+ArgGroup::new("input")
+.required(false)
+.args(& ["from-stdin", "files"])
+)
 )]
 pub struct Config {
     /// Read file paths from stdin
     ///
     /// When this flag is enabled, cw will treat each line in stdin as a file path
-    //
     /// Example:
     ///     $ cat list_files.txt | cw --from-stdin
-    #[clap(long)]
-    pub from_stdin:bool,
+    #[clap(long, verbatim_doc_comment)]
+    pub from_stdin: bool,
 
-    /// Counts line jumps
-    #[clap(short,long)]
+    /// Enables line count
+    #[clap(short, long, verbatim_doc_comment)]
     pub lines: bool,
 
-    /// Counts characters
-    #[clap(short,long = "chars")]
-    pub characters:bool,
+    /// Enables character count
+    #[clap(short, long = "chars", verbatim_doc_comment)]
+    pub characters: bool,
 
-    /// Counts words.
-    ///
-    /// cw count words as defined on ISSPACE(3)
-    #[clap(short,long)]
+    /// Enables word count as defined on ISSPACE(3)
+    #[clap(short, long, verbatim_doc_comment)]
     pub words: bool,
 
-    /// Counts the number of bytes
-    #[clap(short,long)]
-    pub bytes:bool,
+    /// Enables byte count
+    #[clap(short, long, verbatim_doc_comment)]
+    pub bytes: bool,
 
     /// Shows the longest line size
-    #[clap(short= 'L',long)]
-    pub line_length:bool,
+    #[clap(short = 'L', long, verbatim_doc_comment)]
+    pub line_length: bool,
 
     /// Use multithreading
     ///
@@ -54,16 +53,15 @@ pub struct Config {
     ///
     /// The kind of line break cw will search for. It can be LF '\n' or CR '\r'.
     /// For Windows' CRLF files either should work fine
-    #[clap(short,long,default_value_t)]
-    pub newline:LineBreak,
+    #[clap(short, long, default_value_t)]
+    pub newline: LineBreak,
 
     /// Character encoding expected
     ///
     /// The file encoding used on these files. Posible values are UTF8 and UTF16
-    /// Both UTF16 variants, Little Endian and Big Endian are supported out of
-    /// the box
-    #[clap(short,long,default_value_t)]
-    pub encoding:Encoding,
+    /// Both UTF16 variants, Little Endian and Big Endian, are supported
+    #[clap(short, long, default_value_t)]
+    pub encoding: Encoding,
 
     /// List of input files to analyze
     ///
@@ -71,4 +69,34 @@ pub struct Config {
     /// `from-stdin` option
     #[clap()]
     pub files: Vec<String>,
+}
+
+impl From<Config> for CwParser {
+    fn from(config: Config) -> Self {
+        let Config {
+            lines,
+            characters,
+            words,
+            bytes,
+            line_length,
+            newline,
+            encoding,
+            ..
+        } = config;
+        let is_custom = [lines, characters, words, bytes, line_length].contains(&true);
+
+        if is_custom {
+            CwParser::new(
+                encoding,
+                newline,
+                lines,
+                words,
+                characters,
+                bytes,
+                line_length,
+            )
+        } else {
+            CwParser::new(encoding, newline, true, true, false, true, false)
+        }
+    }
 }
--- crates/cw/src/main.rs
+++ crates/cw/src/main.rs
@@ -13,75 +13,28 @@
 //! To learn more about this project, visit it's [GitHub repo](https://github.com/Altair-Bueno/cw)
 //!
 use clap::Parser;
-
 use config::Config;
-use exec_jobs::*;
 use libcw::Parser as CwParser;
-use tokio::io::AsyncBufReadExt;
 
 mod config;
-mod exec_jobs;
+mod run;
 
 #[cfg_attr(feature = "mimalloc", global_allocator)]
 #[cfg(feature = "mimalloc")]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-fn config_to_parser(config: &Config) -> CwParser {
-    let Config {
-        lines,
-        characters,
-        words,
-        bytes,
-        line_length,
-        newline,
-        encoding,
-        ..
-    } = config;
-    let is_custom = [lines, characters, words, bytes, line_length].contains(&&true);
-
-    if is_custom {
-        CwParser::new(
-            *encoding,
-            *newline,
-            *lines,
-            *words,
-            *characters,
-            *bytes,
-            *line_length,
-        )
-    } else {
-        CwParser::new(*encoding, *newline, true, true, false, true, false)
-    }
-}
-
 #[tokio::main(flavor = "current_thread")]
 async fn current_thread_flavour(config: Config, parser: CwParser) -> i32 {
-    run(config, parser).await
+    crate::run::run(config, parser).await
 }
 
 #[tokio::main]
 async fn multiple_threads_flavour(config: Config, parser: CwParser) -> i32 {
-    run(config, parser).await
+    crate::run::run(config, parser).await
 }
-
-async fn run(config: Config, parser: CwParser) -> i32 {
-    if !config.files.is_empty() {
-        let iterable = config.files.into_iter().map(Ok);
-        let stream = tokio_stream::iter(iterable);
-        process_files(stream, parser).await
-    } else if config.from_stdin {
-        let stdin = tokio::io::stdin();
-        let buf = tokio::io::BufReader::new(stdin);
-        let lines = tokio_stream::wrappers::LinesStream::new(buf.lines());
-        process_files(lines, parser).await
-    } else {
-        process_stdin(parser).await
-    }
-}
-
 fn main() -> ! {
     let config: Config = Config::parse();
-    let parser = config_to_parser(&config);
+    let parser = config.clone().into();
     let out_code = if config.multithread {
         multiple_threads_flavour(config, parser)
     } else {
