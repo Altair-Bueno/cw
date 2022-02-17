82bf810a48054ca93b7b74927f791ceb038f15b0
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-17 15:16:41 +0100

    new(cw): Script now builds completitions

--- crates/cw/build.rs
+++ crates/cw/build.rs
@@ -1,4 +1,26 @@
+use std::env;
+
+use clap::{Command, CommandFactory};
+use clap_complete::{generate_to, Shell};
+
+include!("src/config.rs");
+
+const BIN_NAME:&str = env!("CARGO_PKG_NAME");
+
+const SHELL_LIST:&[Shell] = &[
+    Shell::Bash,
+    Shell::Elvish,
+    Shell::Fish,
+    Shell::PowerShell,
+    Shell::Zsh,
+];
 
 fn main() {
-    println!("Not implemented")
+    let mut command :Command = Config::command();
+    let target_dir = env::var("OUT_DIR").unwrap();
+    for shell in SHELL_LIST {
+        let out_dir = format!("{target_dir}/competitions/{shell}");
+        let _ = std::fs::create_dir_all(&out_dir);
+        generate_to(shell.clone(), &mut command, BIN_NAME, out_dir).unwrap();
+    }
 }
